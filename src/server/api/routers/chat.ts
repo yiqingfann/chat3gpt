664b84aac72a645bc4895dd54ccabb23ef629f42
yiqingfann <yf2602@columbia.edu>
2023-04-09 21:50:18 -0700

    feat: tRPC router for chat

---
+++ src/server/api/routers/chat.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const chatRouter = createTRPCRouter({
+  getResponse: publicProcedure
+    .input(z.object({
+      messages: z.array(z.object({
+        role: z.string(),
+        content: z.string(),
+      }))
+    }))
+    .query(({ input }) => {
+      console.log("---input", input);
+
+      return {
+        message: `Hello from tRPC!!`,
+      };
+    }),
+});
