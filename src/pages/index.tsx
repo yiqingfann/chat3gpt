9019d34fd6ca5973702e27866f4a8dd765981085
yiqingfann <yf2602@columbia.edu>
2023-04-15 00:43:26 -0700

    feat(ui): auto grow and shrink message input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,72 @@
 import { type NextPage } from "next";
-import { type FormEvent, useState, useEffect } from "react";
+import { type FormEvent, type ChangeEvent, useState, useEffect, useRef, type SetStateAction } from "react";
 import type { ChatCompletionResponseMessage, ChatCompletionRequestMessage } from "openai";
 import { api } from "~/utils/api";
+import autosize from "autosize";
+
+type MessageInputProps = {
+  setMessages: React.Dispatch<SetStateAction<ChatCompletionRequestMessage[]>>;
+};
+
+const MessageInput = ({ setMessages }: MessageInputProps) => {
+  const [curUserMessage, setCurUserMessage] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (!textAreaRef.current) return;
+
+    // apply autosize
+    autosize(textAreaRef.current);
+    // limit max height to 8 lines
+    const lineHeight = parseInt(getComputedStyle(textAreaRef.current).lineHeight);
+    textAreaRef.current.style.maxHeight = `${8 * lineHeight}px`
+  }, [textAreaRef]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    // save message to state
+    setCurUserMessage(e.target.value);
+
+    // hide the bottom scrollbar
+    if (!textAreaRef.current) return;
+    textAreaRef.current.style.overflow = "auto";
+  }
+
+  const handleSendMessage = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // update messages state to trigger RPC
+    if (curUserMessage.trim().length) {
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { role: "user", content: curUserMessage },
+      ]);
+    }
+
+    // clean up the textarea
+    setCurUserMessage("");
+    if (!textAreaRef.current) return;
+    textAreaRef.current.style.height = "auto";
+  }
+
+  return (
+    <form
+      onSubmit={handleSendMessage}
+      className="flex"
+    >
+      <textarea
+        className="flex-1 bg-transparent text-white focus:outline-none resize-none"
+        rows={1}
+        ref={textAreaRef}
+        placeholder="Write something..."
+        value={curUserMessage}
+        onChange={handleInputChange}
+      />
+      <button className="px-2 text-white">Send</button>
+    </form>
+  );
+}
 
 const Home: NextPage = () => {
-  const [curUserMessage, setCurUserMessage] = useState("");
   const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([
     // { role: "user", content: "Hello, I'm Frank" },
     // { role: "assistant", content: "Hi, I'm ChatGPT" },
@@ -15,18 +77,6 @@ const Home: NextPage = () => {
     enabled: messages.length > 0 && messages[messages.length - 1]?.role === "user",
     refetchOnWindowFocus: false,
   });
-
-  const handleSendMessage = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (!curUserMessage.trim().length) return;
-
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { role: "user", content: curUserMessage },
-    ]);
-    setCurUserMessage("");
-  }
 
   useEffect(() => {
     if (!data) return;
@@ -51,21 +101,9 @@ const Home: NextPage = () => {
         })}
       </div>
 
-      <div className="absolute bottom-0 sm:bottom-12 left-0 right-0">
-        <div className="bg-[#40414F] container mx-auto p-3 rounded-md">
-          <form
-            onSubmit={handleSendMessage}
-            className="flex"
-          >
-            <textarea
-              className="flex-1 bg-transparent text-white focus:outline-none resize-none h-auto"
-              rows={1}
-              placeholder="Write something..."
-              value={curUserMessage}
-              onChange={(e) => setCurUserMessage(e.target.value)}
-            />
-            <button className="px-2 text-white">Send</button>
-          </form>
+      <div className="absolute bottom-0 sm:bottom-12 left-0 right-0 flex justify-center">
+        <div className="bg-[#40414F] container p-3 rounded-md">
+          <MessageInput setMessages={setMessages} />
         </div>
       </div>
 
