84b5e637f0be7468ecdd13fdfc8ad36de2ee2013
yiqingfann <yf2602@columbia.edu>
2023-04-27 21:50:18 -0700

    persist conversation with correct userId

--- src/pages/api/conversations.ts
+++ src/pages/api/conversations.ts
@@ -1,3 +1,4 @@
+import { getAuth } from "@clerk/nextjs/server";
 import { PrismaClient } from "@prisma/client";
 
 import type { NextApiRequest, NextApiResponse } from "next";
@@ -5,7 +6,11 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, rsp: NextApiResponse) => {
-  const conversation = await prisma.conversation.create({ data: { userId: "frank" } });
+  const { userId } = getAuth(req);
+  if (!userId) return rsp.status(401).json({ error: "Unauthorized" });
+
+  // save new conversation to database
+  const conversation = await prisma.conversation.create({ data: { userId: userId } });
 
   return rsp.status(200).json({ conversation });
 }
