486453fc0d3f823d082cd9c85903c5e7ef955782
yiqingfann <yf2602@columbia.edu>
2023-05-07 21:51:42 -0700

    feat: home page font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,23 +2,33 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 
+import { Montserrat } from "@next/font/google"
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+})
+
 const Home: NextPage = () => {
   return (
-    <div className="absolute top-0 bottom-0 left-0 right-0 bg-primary text-white flex flex-col justify-center items-center">
-      <div className="text-7xl">ChaT3Gpt</div>
+    <div className={`absolute top-0 bottom-0 left-0 right-0 bg-primary text-white flex flex-col justify-center items-center ${montserrat.className}`}>
+      <div className="text-5xl sm:text-7xl font-black">
+        Cha<span className="text-accent">T3</span>Gpt
+      </div>
 
       <div className="h-8" />
 
-      <div>A ChatGPT Clone built with T3 Stack. Have Fun!</div>
+      <div className="text-xs sm:text-lg">
+        A ChatGPT Clone built with T3 Stack. Have Fun!
+      </div>
 
       <div className="h-8" />
 
       <Image src="/chat-logo.svg" alt="me" width="256" height="256" />
 
-      <div className="h-8" />
+      <div className="h-10" />
 
       <Link href="/chat">
-        <button className="bg-accent px-8 py-2 rounded-md text-lg text-black">
+        <button className="bg-accent px-8 py-2 rounded-md text-lg text-black font-semibold">
           Start
         </button>
       </Link>
